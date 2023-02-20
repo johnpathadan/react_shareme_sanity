196e2139b38b5a9e1e9820ef8c96cb19d8c6dde1
johnpathadan <johnpathadan@gmail.com>
2023-02-20 16:10:31 +0530

    fix: tailwind installed

---
+++ shareme_frontend/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { ReactDOM } from "react-dom"; //what is react-dom
+import App from "./App";
+import './index.css';
+
+ReactDOM.render(<App />, document.getElementById("root"));
+//^ here we are hooking the app  with the real project id 'root'
